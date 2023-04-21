dd82aee7d5604257f7e03ced994e4afa994bc8f4
Nekomane07 <113617933+Nekomane07@users.noreply.github.com>
2023-04-21 12:13:16 -0500

    2 files modified

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var cupcakeDisplay = document.getElementById('cupcakeDisplay')
 var moneyDisplay = document.getElementById('moneyDisplay')
 var income = document.getElementById('income')
 var employments = document.getElementById('employments')
+var valueUpgrade = document.getElementById('valueUpgrade')
 var title = document.getElementById('title')
 var sell = document.getElementById('trade')
 let cupcakes = 0
@@ -17,17 +18,29 @@ let mps = 0
 let investmentPrice = 100
 let businessPrice = 1000
 let employeePrice = 500
+let valuePrice = 10000
 let interval = 300
 let employees = 0
 
 function myTimer() {
   cupcakes += (mpc/4)*(mps * employees)
   cupcakeDisplay.innerHTML = 'Cupcakes: ' + Math.floor(cupcakes)
+  moneyDisplay.innerHTML = 'Cash: ' + Math.floor(money) + '$';
   title.innerHTML = Math.round(cupcakes) + ' - ' + 'Kitty Clicker'
   income.innerHTML = 'Income: ' + Math.floor(((mpc/4)*(mps*employees)*100))/100 + '$'
-
+  sell.innerHTML = 'Sell: ' + Math.floor(((cupcakes*cupcakeValue)*100))/100 + '$';
 }
 
+
+valueUpgrade.addEventListener("click", function() {
+  if (money >= valuePrice) {
+    cupcakeValue += 1
+    money -= valuePrice;
+    valuePrice = Math.round(employeePrice*1.25);
+    valueUpgrade.innerHTML = 'Cupcake Value: ' + cupcakeValue + ' | ' + valuePrice + '$';
+    moneyDisplay.innerHTML = 'Cash: ' + Math.floor(money) + '$';
+  }
+}​);​
 
 employments.addEventListener("click", function() {
   if (money >= employeePrice) {
@@ -35,7 +48,7 @@ employments.addEventListener("click", function() {
     money -= employeePrice;
     employeePrice = Math.round(employeePrice*1.5);
     employments.innerHTML = 'Kitty Bakers: ' + employees + ' | ' + employeePrice + '$';
-    moneyDisplay.innerHTML = 'Cash: ' + Math.floor(money);
+    moneyDisplay.innerHTML = 'Cash: ' + Math.floor(money) + '$';
   }
 }​);​
 
@@ -44,15 +57,16 @@ clicker.addEventListener("click", function() {
   cupcakeDisplay.innerHTML = 'Cupcakes: ' + Math.floor(cupcakes);
   console.log(cupcakes)
   investment.innerHTML = 'Cupcake Mix: ' + mpc + ' | ' + investmentPrice + '$';
+  sell.innerHTML = 'Sell: ' + Math.floor(((cupcakes*cupcakeValue)*100))/100 + '$'
 }​);​
 
 investment.addEventListener("click", function() {
-  if (cupcakes >= investmentPrice) {
+  if (money >= investmentPrice) {
     mpc = (Math.floor(( mpc*1.1 )*100))/100;
-    cupcakes -= investmentPrice;
+    money -= investmentPrice;
     investmentPrice = Math.round(investmentPrice*1.2)
     investment.innerHTML = 'Cupcake Mix: ' + mpc + ' | ' + investmentPrice + '$';
-    cupcakeDisplay.innerHTML = 'cupcakes: ' + Math.floor(cupcakes);
+    cupcakeDisplay.innerHTML = 'Cupcakes: ' + Math.floor(cupcakes);
   }
 }​);​
 
@@ -62,13 +76,13 @@ business.addEventListener("click", function() {
       money -= businessPrice;
       businessPrice = Math.round(businessPrice*2);
       business.innerHTML = 'Bakeries: ' + mps + ' | ' + businessPrice + '$';
-      moneyDisplay.innerHTML = 'Cash: ' + Math.floor(money); 
+      moneyDisplay.innerHTML = 'Cash: ' + Math.floor(money) + '$'; 
   }
 }​);​
 
 sell.addEventListener("click", function() {
   money += cupcakes*cupcakeValue;
-  moneyDisplay.innerHTML = 'Cash: ' + Math.floor(cupcakes);
+  moneyDisplay.innerHTML = 'Cash: ' + Math.floor(cupcakes) + '$';
   cupcakes = 0
   cupcakeDisplay.innerHTML = 'Cupcakes: ' + Math.floor(cupcakes);
 }​);​
